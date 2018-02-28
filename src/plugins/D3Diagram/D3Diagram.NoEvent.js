8f9eb634776e3b4b7ae2f6fe1f84f4fa0bc6a522
Rui.Ni <rui.ni@ptmind.com>
2018-02-28 16:09:53 +0800

    demo

--- src/plugins/D3Diagram/D3Diagram.NoEvent.js
+++
@@ -1,9 +0,0 @@
-/**
- * ETL画布阻止冒泡事件
- */
-import { event } from 'd3-selection';
-
-export default function () {
-    event.preventDefault();
-    event.stopImmediatePropagation();
-}
--- src/plugins/vue-d3.js
+++
@@ -1,6 +0,0 @@
-import * as d3 from 'd3';
-import D3Diagram from './D3Diagram/D3Diagram';
-
-export default function install (Vue) {
-    Vue.prototype.D3Diagram = new D3Diagram(d3);
-}
